20be2f12c822201292427c53b272cea99dc150c0
Rasmus Fuhse <fuhse@data-quest.de>
2020-03-12 10:48:23 +0100

    add bing support for testing

--- controllers/translator.php
+++ controllers/translator.php
@@ -8,6 +8,7 @@ class TranslatorController extends PluginController
     {
         $language = explode("_", $_SESSION['_language']);
         $language = strtoupper($language[0]);
+
         $output = [];
 
         switch (Request::get("item_type")) {
@@ -34,6 +35,14 @@ class TranslatorController extends PluginController
                 break;
         }
 
+        //check if we have this in cache already:
+        $cache_hash = "BABELFISH_TEXT_" . md5($language . "__" . $text);
+        $cache = StudipCacheFactory::getCache();
+        $cached_text = $cache->read($cache_hash);
+        if ($cached_text) {
+            return formatReady($cached_text);
+        }
+
         $auth_key = Config::get()->BABELFISH_AUTHKEY;
 
         if (Config::get()->BABELFISH_SERVICE === "DEEPL") {
@@ -50,9 +59,35 @@ class TranslatorController extends PluginController
                 'format' => "text"
             ];
         }
+        if (Config::get()->BABELFISH_SERVICE === "BING") {
+            $url = "https://api-eur.cognitive.microsofttranslator.com/translate?api-version=3.0&textType=html&to=".strtolower($language);
+
+            $payload = [['Text' => $text, 'to' => strtolower($language)]];
+
+            $header = array();
+            $header[] = "Ocp-Apim-Subscription-Key: ".$auth_key;
+            $r = curl_init();
+            curl_setopt($r, CURLOPT_URL, $url);
+            curl_setopt($r, CURLOPT_POST, 1);
+            curl_setopt($r, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($r, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($r, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($r, CURLOPT_SSL_VERIFYHOST, true);
+            curl_setopt($r, CURLOPT_POSTFIELDS, json_encode($payload));
+            $result = curl_exec($r);
+            curl_close($r);
+
+            $result = json_decode($result, true);
+
+            if (!$result['error']) {
+                $text = $result[0]['translations'][0]['text'];
+            }
+        }
         if (Config::get()->BABELFISH_SERVICE === "TEST") {
             $text .= "\n\n(This is a test-output by Rasmus.)";
         }
+
+        $cache->write($cache_hash, $text);
 
         $output['html'] = formatReady($text);
 
